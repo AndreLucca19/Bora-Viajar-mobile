6336a335dddfad973de5c6eabe410e85d65cf2d1
Isabella <isabella.tds1.senai@gmail.com>
2025-04-24 16:29:57 -0300

    feat: adjustment timeline

--- App.js
+++ App.js
@@ -2,12 +2,11 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./pages/Login";
-import Cadastro from "./pages/Cadastro"; 
-// import Tab from "./navigation/BottonTabNavigator";
+import Cadastro from "./pages/Cadastro";
+import Inicial from "./pages/Inicial";
+import Tab from "./navigation/BottonTabNavigator";
 
 const Stack = createStackNavigator();
-
-
 
 export default function App() {
   return (
@@ -16,18 +15,26 @@ export default function App() {
         <Stack.Screen
           name="Login"
           component={Login}
-          options={{ headerShown: false}}
-          /> 
-        <Stack.Screen 
-        name="Cadastro" 
-        component={Cadastro} 
-        options={{headerShown: false}}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Cadastro"
+          component={Cadastro}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Inicial"
+          component={Inicial}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Tab}
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
-      
+
     </NavigationContainer>
-     // <NavigationContainer>
-    //<Tab /> 
-  // </NavigationContainer>
+
   );
 }
--- pages/Inicial.js
+++ pages/Inicial.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native"; 
+import { View, Text, Button } from "react-native"; 
 
-export default function Inicial () {
+export default function Inicial() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Página Menu que contém informações</Text>
+            <Text style={styles.title}>Página Menu que contém informações</Text>         
+            <Button title="Ir para a timeline" onPress={() => navigation.navigate("Home")} />
+
         </View>
     );
 }
